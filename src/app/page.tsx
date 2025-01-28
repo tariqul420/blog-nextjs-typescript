b241395a3edb2cce93439e0586cf4ae0d86ee1bd
MD TARIQUL ISLAM <tariqul.developer@gmail.com>
2025-01-28 11:26:04 +0600

    home page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div>This is a home page</div>;
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId?: number;
+}
+
+export default async function Home() {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts: Post[] = await data.json();
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {posts.map((post) => (
+        <div key={post?.id} className="card bg-base-100  shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">{post?.title}</h2>
+            <p>{post?.body}</p>
+            <div className="card-actions justify-start">
+              <button className="btn btn-primary">See Details</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
